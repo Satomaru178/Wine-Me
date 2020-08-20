b7b8f6e3ebf6e855a86a907b0de7cbec6532302c
Satomaru178 <satoshichangetheworld@yahoo.co.jp>
2020-08-20 14:16:53 +0900

    Create brands table

--- db/migrate/20200820045623_create_brands.rb
+++ db/migrate/20200820045623_create_brands.rb
@@ -1,7 +1,17 @@
 class CreateBrands < ActiveRecord::Migration[5.2]
   def change
     create_table :brands do |t|
-
+      t.string     :name, 	 null: false, index: true, length: { maximum: 40 }
+      t.string     :origin,  null: false
+      t.text       :explain, null: false, length: { maximum: 1_000 }
+      t.string     :year
+      t.string     :value
+      t.integer    :scene
+      t.integer    :taste
+      t.integer    :likes_count
+      t.references :user,    null: false
+      t.references :type
+      t.references :variety
       t.timestamps
     end
   end
