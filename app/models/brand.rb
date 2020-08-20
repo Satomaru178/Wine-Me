8e4b23cfe1225a2917cd025876cd0c5cf18335b5
Satomaru178 <satoshichangetheworld@yahoo.co.jp>
2020-08-20 14:54:20 +0900

    Update barnd.rb about association and validates

--- app/models/brand.rb
+++ app/models/brand.rb
@@ -1,2 +1,16 @@
 class Brand < ApplicationRecord
+
+  belongs_to :user
+  belongs_to :type
+  belongs_to :variety
+  has_many   :likes, dependent: :destroy
+  has_many   :comments, dependent: :destroy
+  has_many   :images, dependent: :destroy
+
+  belongs_to_active_hash :scene
+  belongs_to_active_hash :taste
+
+  validates :name, presence: true, length: { maximum: 40 }
+  validates :explain, length: { maximum: 1_000 }
+
 end
